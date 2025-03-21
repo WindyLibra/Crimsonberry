1c5db163c9e565f04c10b7c049df95dab4335d17
David Makaryan <windylibra3000@gmail.com>
2025-03-21 20:45:47 +0400

    done footer?

--- crimsonberry-next/src/app/page.tsx
+++ crimsonberry-next/src/app/page.tsx
@@ -4,9 +4,16 @@ import Footer from '@/components/footer';
 
 export default function Page() {
   return (
-      <>
+      <div className='flex-col flex min-h-screen'>
+
         <Header text="Home" />
+
+          <main className="flex-grow">
+
+          </main>
+
         <Footer />
-      </>
+
+      </div>
   )
 } 
--- crimsonberry-next/src/components/footer.tsx
+++ crimsonberry-next/src/components/footer.tsx
@@ -2,8 +2,14 @@ import '@/globals.css'
 
 export default function Footer(){
     return(
-        <footer className='bg-amber-50 flex justify-center items-center'>
-            
+        <footer className='w-full'>
+            <div className='flex mx-auto sizer justify-between items-center'> 
+                <img src="/house-solid.svg" width={20} height={20} alt="" />
+                <img src="/magnifying-glass-solid.svg" width={20} height={20} alt="" />
+                <img src="/table-cells-large-solid.svg" width={20} height={20} alt="" />
+                <img src="/print-solid.svg" width={20} height={20} alt="" />
+                <img src="/circle-regular.svg" width={20} height={20} alt="" />
+            </div>
         </footer>
     )
 }
